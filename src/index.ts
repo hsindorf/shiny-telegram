304121bdcecb9b9aca41c82561b2c811436317a9
Hannah Sindorf <hannah@hsindorf.com>
2019-11-09 19:23:42 -0800

    eslint and prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import express = require('express');
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 if (process.env.NODE_ENV !== 'production') {
-	dotenv.config();
+  dotenv.config();
 }
 
 const PORT = process.env.PORT;
 const app: express.Application = express();
 
 app.get('/', (req, res) => {
-    console.log(req);
-    res.send('Hello world');
+  console.log(req);
+  res.send('Hello world');
 });
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})
+  console.log(`App listening on port ${PORT}`);
+});
