33885009984267088f3d7889e805d89d12bd9d86
Hannah Sindorf <hannah@hsindorf.com>
2019-11-09 18:00:52 -0800

    project scaffolding

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express = require('express');
+import * as dotenv from 'dotenv'
+
+if (process.env.NODE_ENV !== 'production') {
+	dotenv.config();
+}
+
+const PORT = process.env.PORT;
+const app: express.Application = express();
+
+app.get('/', (req, res) => {
+    console.log(req);
+    res.send('Hello world');
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+})
